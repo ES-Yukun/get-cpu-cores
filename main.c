b94f029c9fc8c95f7c9ce881a2accc7864ba127a
yush <yukun@team.anylinks.jp>
2023-09-18 01:00:21 +0000

    update

--- main.c
+++ main.c
@@ -4,17 +4,30 @@
 
 int main(int ac, char **av)
 {
+	int av1_len;
+
+	if (ac == 2)
+		av1_len = strlen(av[1]) + 1;
 	if (ac == 1)
 		printf("{"
-			   "\"amount\":\"%d\","
-			   "\"available\":\"%d\""
+			   "\"all\":\"%d\","
+			   "\"enabled\":\"%d\""
 			   "}",
 			   get_nprocs_conf(),
 			   get_nprocs());
-	else if (ac == 2 && 0 == memcmp("amount", av[1], strlen(av[1])))
+	else if (ac == 2 &&
+			 (0 == memcmp("--all", av[1], av1_len) ||
+			  0 == memcmp("-a", av[1], av1_len)))
 		printf("%d", get_nprocs_conf());
-	else if (ac == 2 && 0 == memcmp("avaiable", av[1], strlen(av[1])))
+	else if (ac == 2 &&
+			 (0 == memcmp("--enabled", av[1], av1_len) ||
+			  0 == memcmp("-e", av[1], av1_len)))
 		printf("%d", get_nprocs());
+	else if (ac == 2 && 0 == memcmp("--help", av[1], av1_len))
+		printf("Usage: getcpu [OPTION...]\n"
+		"\n"
+		"  --all, -a\t\tprint amount cpu cores\n"
+		"  --enabled, -e\t\tprint enabled cpu cores\n");
 	else
 		return (1);
 	return (0);
